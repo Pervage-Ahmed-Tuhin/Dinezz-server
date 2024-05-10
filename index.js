0b6544ce62c446e7b6a79990a9957fa92d71986a
Pervage-Ahmed-Tuhin <parvageahmedtuhin123456@gmail.com>
2024-05-10 19:28:08 +0600

    added post of the addded foods

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const jwt = require('jsonwebtoken');
 const cookieParser = require('cookie-parser');
 
@@ -21,6 +22,68 @@ app.use(cors(
 
 app.use(express.json());
 app.use(cookieParser());
+
+
+
+
+
+
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.iz3dvmk.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
+});
+
+async function run() {
+    try {
+
+
+
+        const featuredFoodCollection = client.db('featured').collection('food');
+
+        app.get('/featuredFood', async (req, res) => {
+            const cursor = featuredFoodCollection.find();
+            const result = await cursor.toArray();
+            res.send(result);
+        })
+
+        app.post('/addedFood', async (req, res) => {
+            try {
+                const newFood = req.body;
+                console.log(newFood);
+                const result = await featuredFoodCollection.insertOne(newFood);
+                res.json(result);
+            } catch (error) {
+                console.error(error);
+                res.status(500).json({ message: 'Internal server error' });
+            }
+        })
+
+
+
+
+
+
+        await client.db("admin").command({ ping: 1 });
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    } finally {
+        // Ensures that the client will close when you finish/error
+
+    }
+}
+run().catch(console.dir);
+
+
+
+
+
 
 
 app.get('/', (req, res) => {
